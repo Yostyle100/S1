893ce903409a220db90b75cd1f359d17a91639de
Yostyle100 <maru810210@gmail.com>
2025-01-19 11:17:12 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/zYNn2Vi2bDRz7Ch89',
+	'https://forms.gle/y5B3VTXtaf4A922M8',
 ];
