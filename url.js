cd4c0c06a2586195b4f868b2efd3b7bf5f2323f6
Yostyle100 <maru810210@gmail.com>
2025-05-12 13:39:50 +0800

    Update url.js

--- url.js
+++ url.js
@@ -12,5 +12,6 @@ const urls = [
 	'https://forms.gle/hyg7jou9uVjaTw8R6',
 	'https://forms.gle/RDi18nNcLCir64Qz6',
 	'https://forms.gle/BXiJ3bfunJmDBCm27',
+	'https://forms.gle/SSXWW4JGcYTcaEXQ6',
 	'https://forms.gle/4W5yfQysR3v4uGUz5',
 ];
